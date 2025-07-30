19ab3be48589432c9f3e31d3199c3a3e54ae5018
Shubh <shubhkumar357@gmail.com>
2025-07-30 20:18:36 +0530

    interview prep

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import {dummyInterviews} from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import {getCurrentUser, } from "@/lib/actions/auth.action";
+import {getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/general.action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    const [userInterviews, latestInterviews] = await Promise.all([
+        await getInterviewsByUserId(user?.id!),
+        await getLatestInterviews({ userId: user?.id! })
+    ]);
+
+    const hasPastInterviews = userInterviews?.length > 0;
+    const hasUpcomingInterviews = latestInterviews?.length > 0;
+
     return (
         <>
             <section className="card-cta">
@@ -27,11 +38,12 @@ const Page = () => {
                 <h2>Your Interviews</h2>
 
                 <div className="interviews-section">
-                    {dummyInterviews.map((interview) => (
-                        <InterviewCard {...interview} key={interview.id}/>
-                    ))}
-
-                    {/*<p>You haven&apos;t taken any interviews yet</p>*/}
+                    {hasPastInterviews ? (
+                        userInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                            <p>You haven&apos;t taken any interviews yet</p>
+                    )}
                 </div>
             </section>
 
@@ -39,9 +51,12 @@ const Page = () => {
                 <h2>Take an Interview</h2>
 
                 <div className="interviews-section">
-                    {dummyInterviews.map((interview) => (
-                        <InterviewCard {...interview} key={interview.id} />
-                    ))}
+                    {hasUpcomingInterviews ? (
+                        latestInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                        <p>There are no new interviews available</p>
+                    )}
                 </div>
             </section>
         </>
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiLoLPwyFGMTqCPyWTO4wSwrqQrE-xcL4",
-  authDomain: "synapse-ee601.firebaseapp.com",
-  projectId: "synapse-ee601",
-  storageBucket: "synapse-ee601.firebasestorage.app",
-  messagingSenderId: "391774424232",
-  appId: "1:391774424232:web:634471a0409272e20e069d",
-  measurementId: "G-005CHS3L7C"
+    apiKey: "AIzaSyA2IBfcG1WD-zQ1hZDXOa-_mt9W8c0QOJ0",
+    authDomain: "prepwise-4287a.firebaseapp.com",
+    projectId: "prepwise-4287a",
+    storageBucket: "prepwise-4287a.firebasestorage.app",
+    messagingSenderId: "718591450179",
+    appId: "1:718591450179:web:829d057d79dcd3a761c8f6",
+    measurementId: "G-RHTQ8XQHR5"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app)
